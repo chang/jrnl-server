0615f15a4aff8f38c809ed35e809fc16df714089
Eric <ericchang00@gmail.com>
2019-01-13 22:32:29 -0800

    use global flask config

--- jrnl_server/config.py
+++ jrnl_server/config.py
@@ -3,10 +3,20 @@ import jrnl
 
 
 class Config:
+
+    # Name of the author to be used in page headers.
+    NAME = 'Eric'
+
+    # Text displayed below page title.
+    SUBTITLE = 'Hello world!'
+
+    # Path to .jrnl_config.
     CONFIG_PATH = os.path.join(os.environ['HOME'], '.jrnl_config')
 
+    # Name of the journal to render.
     JOURNAL_NAME = 'default'
 
+    # Colors to give tags. Can be either Bulma colors or hexadecimal colors.
     TAG_COLORS = {
         '@tag': 'info',
     }
