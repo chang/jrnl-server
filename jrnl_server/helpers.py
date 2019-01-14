703675a51d7c035d1ad6be4f993b2a9aa95172e8
Eric <ericchang00@gmail.com>
2019-01-14 01:29:34 -0800

    render links with titles

--- jrnl_server/helpers.py
+++ jrnl_server/helpers.py
@@ -1,7 +1,19 @@
+import logging
+import os
 from functools import wraps
 from timeit import default_timer
 
 from jrnl_server.config import conf
+
+
+def reload_on_change(journal):
+    def intermediate_decorator(func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            journal.reload_if_changed()
+            return func(*args, **kwargs)
+        return decorated
+    return intermediate_decorator
 
 
 def get_day_with_suffix(day):
@@ -29,6 +41,53 @@ def get_day_with_suffix(day):
         suffix_day -= 1
 
 
+
+def get_link_title(link):
+    """Given a URL, return its title.
+
+    If the title doesn't exist in the cache, asynchronously populate it so it's
+    available on the next pageload.
+    """
+    from threading import Thread
+    import requests
+    import yaml
+    from lxml.html import fromstring
+
+    CACHE_PATH = '/tmp/jrnl_links.yaml'
+    if os.path.exists(CACHE_PATH):
+        with open(CACHE_PATH, 'r') as fp:
+            cache = yaml.load(fp)
+    else:
+        cache = {}
+
+
+    def get_title(link):
+        RANDOM_STRING = 'asdfjsafieowjfklanwknfasdlfvzxcvxz'
+        try:
+            response = requests.get(link, headers={'User-Agent': RANDOM_STRING})
+            tree = fromstring(response.content)
+            title = tree.findtext('.//title')
+            return title
+        except Exception as e:
+            logging.warning('Failed to GET: "{}"'.format(link))
+            logging.warning(e)
+            raise e
+
+
+    def write_title_to_cache(link):
+        cache[link] = get_title(link)
+        with open(CACHE_PATH, 'w') as fp:
+            fp.write(yaml.dump(cache, default_flow_style=False))
+
+
+    if link in cache:
+        return cache[link]
+    else:
+        thread = Thread(target=write_title_to_cache, kwargs={'link': link})
+        thread.start()
+        return link
+
+
 def time_function(func):
     def decorated(*args, **kwargs):
         t0 = default_timer()
@@ -38,16 +97,6 @@ def time_function(func):
         print('Function "{}": {}s'.format(func.__name__, elapsed))
         return result
     return decorated
-
-
-def reload_on_change(journal):
-    def intermediate_decorator(func):
-        @wraps(func)
-        def decorated(*args, **kwargs):
-            journal.reload_if_changed()
-            return func(*args, **kwargs)
-        return decorated
-    return intermediate_decorator
 
 
 def populate_config(app):
